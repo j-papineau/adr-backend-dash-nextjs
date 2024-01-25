20cc0ed2950be4a6abcc97f18fd627e0967192b3
Joel Papineau <118376438+j-papineau@users.noreply.github.com>
2024-01-25 13:49:57 -0500

    begin work on kml converter

--- components/jsonProcessor/JsonConverter.jsx
+++ components/jsonProcessor/JsonConverter.jsx
@@ -19,56 +19,26 @@ const JsonConverter = () => {
   },[file])
 
   const processFile = () => {
-      var json = null;
       setFileProcessing(true);
-      
-      const fr = new FileReader();
-      
-      fr.readAsText(file, "UTF-8");
-      fr.onload = async e => {
-        const content = e.target.result;
-        json = JSON.parse(content);
-        //json is now in object, remove all name attributes
-        let features = json.features
-        console.log(features)
-        var newFeatures = []
-        await features.forEach(feature => {
-          if (feature.geometry.type != 'Polygon' && feature.geometry.type != "MultiPolygon"){
-            
-          }else{
-            feature.properties.Name = ""
-            feature.properties.description = ""
-            newFeatures.push(feature);
-          }
-        });
-        json.features = newFeatures;
-        console.log(json.features)
-        //create and download file
-        let jsonString = JSON.stringify(json)
-        const blob = new Blob([jsonString], {type:'application/json'})
-        //maybe await
-        const href = URL.createObjectURL(blob);
-        const link = document.createElement('a')
-        link.href = href;
-        link.download = "geoJSON.json"
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-        setFileProcessing(false);
-      }
+      fileDone(false)
   }
 
 
 
   return (
     <div className='h-full w-full flex flex-col justify-center items-center'>
-      <div className='flex flex-col space-y-4 h-[40vh] w-[40%] bg-slate-300 rounded-md mx-4 p-2 shadow-md shadow-black items-center'>
+      <div className='flex flex-col space-y-4 p-4 bg-slate-300 rounded-md mx-4 p-2 shadow-md shadow-black items-center'>
+        <p className='text-xl'>KML to Json</p>
         <TextField type='file' onChange={(e) => {
           setFile(e.target.files[0])
         }}/>
         <Button disabled={!buttonEnabled} variant="contained" onClick={processFile}>Process File</Button>
         {fileProcessing ? (<CircularProgress/>): (<></>)}
       </div>
+      {!fileDone ? (<></>) : (
+      <>
+      </>
+      )}
     </div>
 
   )
